e66f42e050de187924dcde96bab12e0a9f6b7a4f
Artanis <ledragio@gmail.com>
2024-04-21 16:16:40 -0400

    Touchups

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import argparse
 import os
+import time
+from tqdm import tqdm
 from src.apkpure import download_apk
-from src.unpack import main_container
+from src.unpack import get_new_count, unpack_all_assets, get_count
 from threading import Thread
 from datetime import datetime
 from zipfile import ZipFile
@@ -11,7 +13,7 @@ parser = argparse.ArgumentParser(description="Download Nova Empire APK and extra
 parser.add_argument("-force_download", "--force_download", action="store_true", help="Redownload APK File. If not set, the script will scan the directory for latest version of .apk file and use it.", default=False)
 parser.add_argument("-d_path", "--download_path", type=str, help="Path to download the APK files. If not set, the script will download the APK file to the current directory.", default=os.getcwd())
 parser.add_argument("-apk", "--apk_path", type=str, help="Path to the APK file. If not set, the script will follow the logic of the 'force_download' parameter.", default=None)
-parser.add_argument("-app_id", "--app_id", type=str, help="App ID of the APK file to download.", default="com.gamebeartech.nova")
+parser.add_argument("-app_id", "--app_id", type=str, help="App ID of the APK file to download. (set by default for nova empire)", default="com.gamebeartech.nova")
 
 args = parser.parse_args()
 
@@ -42,10 +44,18 @@ if not force_download:
     
     if len(apk_files) > 0:
         apk_path = os.path.join(download_path, apk_files[0])
-        print(f"------\nLatest APK file found: {apk_path} - {round(mtimes[0] / 86400, 5)} days old\nSkipping fresh download.\n")
+        print(f"------\nLatest APK file found: {apk_path} - {round((time.time() - mtimes[0]) / 86400, 5)} days old\nSkipping fresh download.\n")
         apk_found = True
     else:
         print(f"------\nNo APK files found in the current directory for {app_id}\nDownloading fresh version...\n")
+
+if force_download and os.path.exists(apk_path):
+    input_resp = input(f"------\nWARNING - File already exists: {apk_path}\nWARNING - Do you want to overwrite the file? (y/n): ").lower()
+    if input_resp == "y":
+        os.remove(apk_path)
+    else:
+        print(f"------\nWARNING - File already exists, cannot force download: {apk_path}\nWARNING - Exiting program.\n")
+        exit()
 
 if not apk_found:
     download_apk("com.gamebeartech.nova", apk_path)
@@ -70,8 +80,26 @@ ZipFile(apk_path).extractall(os.path.join(download_path, "apk_extracted"))
 
 print('------\nExtracted APK file to folder: ' + os.path.join(download_path, 'apk_extracted') + '\nStarting Unity extraction\n')
 
-bin = Thread(target=main_container, args=(os.path.join(download_path, "apk_extracted", "assets", "bin", "Data"), os.path.join(download_path, "EXTRACTED_MEDIA", "misc"), 1))
-main = Thread(target=main_container, args=(os.path.join(download_path, "apk_extracted", "assets", "asset_bundles", "android"), os.path.join(download_path, "EXTRACTED_MEDIA", "main"), 2))
+if os.path.exists(os.path.join(download_path, "EXTRACTED_MEDIA")):
+    shutil.rmtree(os.path.join(download_path, "EXTRACTED_MEDIA"), ignore_errors=True)
+
+os.mkdir(os.path.join(download_path, "EXTRACTED_MEDIA"))
+os.mkdir(os.path.join(download_path, "EXTRACTED_MEDIA", "main"))
+os.mkdir(os.path.join(download_path, "EXTRACTED_MEDIA", "misc"))
+
+bin = Thread(target=unpack_all_assets, args=(os.path.join(download_path, "apk_extracted", "assets", "bin", "Data"), os.path.join(download_path, "EXTRACTED_MEDIA", "misc")))
+main = Thread(target=unpack_all_assets, args=(os.path.join(download_path, "apk_extracted", "assets", "asset_bundles", "android"), os.path.join(download_path, "EXTRACTED_MEDIA", "main")))
 
 bin.start()
 main.start()
+
+bar = tqdm(unit='images', unit_scale=True)
+
+while bin.is_alive() or main.is_alive():
+    bar.update(get_new_count())
+    time.sleep(0.5)
+
+bar.close()
+
+print(f"------\nFINISHED - Total images exported: {get_count()}\n")
+    
--- src/unpack.py
+++ src/unpack.py
@@ -2,76 +2,62 @@ import os
 import UnityPy
 from tqdm import tqdm
 
+global count, last_run
+last_run = 0
+count = 0
 
-
-
-
-
-def main_container(source_folder : str, destination_folder : str, row_n = 1):
-    global main_loop 
+def get_new_count():
+    global count, last_run
     
-    main_loop = True
+    count_val = count
     
+    to_add = count_val - last_run
+    last_run = count_val
     
-    
+    return to_add
 
-    def unpack_all_assets(source_folder : str, destination_folder : str, row_n = 1):
+def get_count():
+    global count
+    return count
+
+def unpack_all_assets(source_folder : str, destination_folder : str):
+    global count
     
-        print_counter = False
-        count = 0
-        if main_loop:
-            if not os.path.exists(destination_folder):
-                if not os.path.exists(os.path.dirname(destination_folder)):
-                    os.mkdir(os.path.dirname(destination_folder))
-                os.mkdir(destination_folder)
-            print_counter = True
-            main_loop = False
-        folder_name = destination_folder
-        if not os.path.exists(folder_name):
-            os.mkdir(folder_name)
-        for file_name in os.listdir(source_folder):
-            if os.path.isdir(os.path.join(source_folder, file_name)):
-                # /android/icon
-                number_added = unpack_all_assets(os.path.join(source_folder, file_name), os.path.join(destination_folder, file_name))
-                if print_counter:
-                    count += number_added
-                continue
-                
-            if file_name.endswith(".manifest") or file_name.endswith(".json") or file_name.endswith(".xml"):
-                continue
-        
-            unity_obj = UnityPy.load(os.path.join(source_folder, file_name))
+    folder_name = destination_folder
+    if not os.path.exists(folder_name):
+        os.mkdir(folder_name)
+    for file_name in os.listdir(source_folder):
+        if os.path.isdir(os.path.join(source_folder, file_name)):
+            # /android/icon
+            number_added = unpack_all_assets(os.path.join(source_folder, file_name), os.path.join(destination_folder, file_name))
+            continue
             
-            for obj in unity_obj.objects:
-                if obj.type.name not in ["Texture2D", "Texture2DArray"]:
-                    continue
-                
-                if obj.type.name == "Texture2D":
-                    image = obj.read()
+        if file_name.endswith(".manifest") or file_name.endswith(".json") or file_name.endswith(".xml"):
+            continue
+    
+        unity_obj = UnityPy.load(os.path.join(source_folder, file_name))
+        
+        for obj in unity_obj.objects:
+            if obj.type.name not in ["Texture2D", "Texture2DArray"]:
+                continue
+            
+            if obj.type.name == "Texture2D":
+                image = obj.read()
+                try:
+                    image.image.save(os.path.join(destination_folder, (image.m_Name + ".png").replace(" ", "_")))
+                    count += 1
+                except:
+                    pass
+            else:
+                for i, image in enumerate(obj.read()):
                     try:
-                        image.image.save(os.path.join(destination_folder, (image.m_Name + ".png").replace(" ", "_")))
-                        if print_counter:
-                            count += 1
+                        image.image.save(os.path.join(destination_folder, (image.m_Name + str(i) + ".png").replace(" ", "_")))
+                        count += 1
                     except:
                         pass
-                else:
-                    for i, image in enumerate(obj.read()):
-                        try:
-                            image.image.save(os.path.join(destination_folder, (image.m_Name + str(i) + ".png").replace(" ", "_")))
-                            if print_counter:
-                                count += 1
-                        except:
-                            pass
-        
-        if len(os.listdir(destination_folder)) == 0:
-            try:
-                os.rmdir(destination_folder)
-            except: 
-                pass
-        
-        if print_counter:
-            print("Total images exported: " + str(count))
-        
-        return count
     
-    unpack_all_assets(source_folder, destination_folder, row_n)
+    if len(os.listdir(destination_folder)) == 0:
+        try:
+            os.rmdir(destination_folder)
+        except: 
+            pass
